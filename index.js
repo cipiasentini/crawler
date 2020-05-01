bd4726bd65e0860a21f5ae0b9b91f95173808f2b
Carlos <cip@pop-os.localdomain>
2020-05-01 14:38:13 -0300

    parseo de opósiciones y vistas

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
 
-server = app.listen(process.env.PORT || 3000, () => console.log(`Listening on ${process.env.PORT || 3000}`))
+server = app.listen(process.env.PORT || 4000, () => console.log(`Listening on ${process.env.PORT || 4000}`))
 
 app.get('/', (req,res) => {res.send("Hola")})
 
@@ -49,67 +49,106 @@ const c = new Crawler({
                         }
                     })
 
-                    // **** OPOSICIONES TABLA ****
+                    // **** SCRIPTS DE LA WEB ****
 
-                    // Por aluguna razon no toma nada del tbody
-                    // console.log($("#tblGrillaopos").html()) // solo devuelve thead
+                    let text = $($('script')).text();
 
-                    let oposiciones = []
-                    let op_obj = {}
-                    
-                    // Crea el objeto de claves vacias
-                    $("#tblGrillaopos > thead > tr > th").each((i, elem) => {
-                        let campo = elem.childNodes[0].nodeValue.trim()
-                        campo = slugify(campo.replace(":","").replace("\.",""), "_")
-                        op_obj[campo] = ""
-                    })
+                    // **** OPOSICIONES TABLA DESDE VARIABLE JS ****
 
-                    // Asigna valores a los objetos, 1..n 1..n oposiciones
-                    $("#tblGrillaopos > tbody > tr").each((i, elem) => {
-                        let new_opo = op_obj
-                        let vals = []
-                        $(elem).children('td').each((j, op) => {
-                            let valor = $(op).html().trim()
-                            vals.push(valor)
-                        })
-                        let index = 0;
-                        for (var prop in new_opo) {
-                            if (Object.prototype.hasOwnProperty.call(new_opo, prop)) {
-                                new_opo[prop] = valor[index]
+                    let list_oposiciones = [];
+                    let var_ops = obtenerValorVariableJS(text,"var opos = ");
+                    let oposiciones = eval(var_ops);
+                    oposiciones.forEach(oposicion => {
+                        let n_op = {};
+                        for (var prop in oposicion) {
+                            if (Object.prototype.hasOwnProperty.call(oposicion, prop)) {
+                                try {
+                                    n_op[prop] = getFormattedVals(oposicion[prop]);
+                                } catch {
+                                    n_op[prop] = oposicion[prop];
+                                }
                             }
-                            index++
                         }
-                        oposiciones.push(new_opo)
-                    })
+                        list_oposiciones.push(n_op);
+                    });
+                    obj["OPOSICIONES"] = list_oposiciones;
 
-                    // **** VISTAS TABLA ****
-                    let vistas = []
-                    let view_obj = {}
-                    
-                    // Crea el objeto de claves vacias
-                    $("#tblGrillaVistas > thead > tr > th").each((i, elem) => {
-                        let campo = elem.childNodes[0].nodeValue.trim()
-                        campo = slugify(campo.replace(":","").replace("\.",""), "_")
-                        view_obj[campo] = ""
-                    })
-
-                    // Asigna valores a los objetos, 1..n vistas
-                    $("#tblGrillaVistas > tbody > tr").each((i, elem) => {
-                        let new_view = view_obj
-                        let vals = []
-                        $(elem).children('td').each((j, v) => {
-                            let valor = $(v).html().trim()
-                            vals.push(valor)
-                        })
-                        let index = 0;
-                        for (var prop in new_view) {
-                            if (Object.prototype.hasOwnProperty.call(new_view, prop)) {
-                                new_view[prop] = valor[index]
+                    // **** VISTAS TABLA DESDE VARIABLE JS ****
+                    let list_vistas = [];
+                    var var_vis = obtenerValorVariableJS(text,"var vistas = ");
+                    var vistas = eval(var_vis);
+                    vistas.forEach(vista => {
+                        let n_vis = {};
+                        for (var prop in vista) {
+                            if (Object.prototype.hasOwnProperty.call(vista, prop)) {
+                                try {
+                                    n_vis[prop] = getFormattedVals(vista[prop]);
+                                } catch {
+                                    n_vis[prop] = vista[prop];
+                                }
                             }
-                            index++
                         }
-                        vistas.push(new_view)
-                    })
+                        list_vistas.push(n_vis);
+                    });
+                    obj["VISTAS"] = list_vistas;
+
+                    insertInDB(obj)
+
+                    // let oposiciones = []
+                    // let op_obj = {}
+                    // // Crea el objeto de claves vacias
+                    // $("#tblGrillaopos > thead > tr > th").each((i, elem) => {
+                    //     let campo = elem.childNodes[0].nodeValue.trim()
+                    //     campo = slugify(campo.replace(":","").replace("\.",""), "_")
+                    //     op_obj[campo] = ""
+                    // })
+
+                    // // Asigna valores a los objetos, 1..n 1..n oposiciones
+                    // $("#tblGrillaopos > tbody > tr").each((i, elem) => {
+                    //     let new_opo = op_obj
+                    //     let vals = []
+                    //     $(elem).children('td').each((j, op) => {
+                    //         let valor = $(op).html().trim()
+                    //         vals.push(valor)
+                    //     })
+                    //     let index = 0;
+                    //     for (var prop in new_opo) {
+                    //         if (Object.prototype.hasOwnProperty.call(new_opo, prop)) {
+                    //             new_opo[prop] = valor[index]
+                    //         }
+                    //         index++
+                    //     }
+                    //     oposiciones.push(new_opo)
+                    // })
+
+                    // // **** VISTAS TABLA ****
+                    // let vistas = []
+                    // let view_obj = {}
+                    
+                    // // Crea el objeto de claves vacias
+                    // $("#tblGrillaVistas > thead > tr > th").each((i, elem) => {
+                    //     let campo = elem.childNodes[0].nodeValue.trim()
+                    //     campo = slugify(campo.replace(":","").replace("\.",""), "_")
+                    //     view_obj[campo] = ""
+                    // })
+
+                    // // Asigna valores a los objetos, 1..n vistas
+                    // $("#tblGrillaVistas > tbody > tr").each((i, elem) => {
+                    //     let new_view = view_obj
+                    //     let vals = []
+                    //     $(elem).children('td').each((j, v) => {
+                    //         let valor = $(v).html().trim()
+                    //         vals.push(valor)
+                    //     })
+                    //     let index = 0;
+                    //     for (var prop in new_view) {
+                    //         if (Object.prototype.hasOwnProperty.call(new_view, prop)) {
+                    //             new_view[prop] = valor[index]
+                    //         }
+                    //         index++
+                    //     }
+                    //     vistas.push(new_view)
+                    // })
                     // insertInDB(obj)
                 }
                 catch(e){
@@ -121,6 +160,43 @@ const c = new Crawler({
     }
 });
 
+function obtenerValorVariableJS(target, variable){
+    var recortar = target.substring(target.search(variable)+variable.length,target.length);
+    var result = recortar.substring(0,recortar.search(";"));
+    return result;
+}
+
+function getFormattedVals(possible_string_date) {
+    if (possible_string_date != "" || possible_string_date != undefined || possible_string_date != null) {
+        // me fijo si no es un numero primero
+        var possible_number = parseInt(possible_string_date.slice(1,-1));
+        if (!isNaN(possible_number)) {
+            return possible_string_date
+        }
+        var date = possible_string_date.slice(6, -2); // si puede hacer esto es o bien fecha (valida, invalida), o bien string
+        date = parseInt(date)
+        // es string, devuelvo tal cual
+        if (isNaN(date)) {
+            return possible_string_date;
+        }
+        // no posee fecha (da negativo cuando no tiene en el script de la web)
+        if (date < 0) {
+            return ""
+        }
+        
+        date = new Date(date);
+        var year = date.getFullYear();
+        
+        var month = (1 + date.getMonth()).toString();
+        month = month.length > 1 ? month : '0' + month;
+        
+        var day = date.getDate().toString();
+        day = day.length > 1 ? day : '0' + day;
+        
+        return year + '-' + month + '-' + day;
+    }
+    return possible_string_date
+}
 const reschedule = async () => {
     // c.queue([{
     //     uri: 'https://portaltramites.inpi.gob.ar/MarcasConsultas/Resultado?acta=2602501',
